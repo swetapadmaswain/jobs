f9b221334a3f36a9e29356dd149eaf4c58a3f554
swetapadmaswain <swetapadma2693@gmail.com>
2025-06-19 13:48:23 +0530

    qq

--- terraform/assignment.tf
+++ terraform/assignment.tf
@@ -1,4 +1,13 @@
 # Configure the AWS Provider
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.0.0"
+    }
+  }
+}
 provider "aws" {
   region = "us-east-1" # You can change your desired AWS region here
 }
